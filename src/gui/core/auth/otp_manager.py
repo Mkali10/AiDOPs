4c6650d491366105874ab561c0313a1830079af5
Ankit vishwa <aviatankit@gmail.com>
2025-12-25 17:01:40 +0530

    Delete src/gui/core/auth directory

--- src/gui/core/auth/otp_manager.py
+++
@@ -1,8 +0,0 @@
-import random
-otp_store={}
-def generate_otp(mobile):
-    code=str(random.randint(1000,9999))
-    otp_store[mobile]=code
-    return code
-def verify_otp(mobile,code):
-    return otp_store.get(mobile)==code
